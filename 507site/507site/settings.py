b78f5e210a8f529631051a3919a965b8f98c3faa
David-Coulter <david.coulter2@hotmail.com>
2026-02-21 18:18:12 -0600

    Fix the settings and urls and include html files

--- 507site/507site/settings.py
+++ 507site/507site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "main", 
+]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -53,7 +55,7 @@ ROOT_URLCONF = "507site.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- 507site/507site/urls.py
+++ 507site/507site/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path("main/", include("main.urls")),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('main/', include('main.urls')),
+    path('', lambda request: redirect('dashboard')),
 ]
