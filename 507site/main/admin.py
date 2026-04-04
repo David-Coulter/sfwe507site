24928de38740370513c782cf6de428fea5c68971
David-Coulter <david.coulter2@hotmail.com>
2026-04-04 17:26:09 -0500

    Complete user story 7 - product backlog view

--- 507site/main/admin.py
+++ 507site/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Task, Comment
+from .models import Task, Comment, Sprint
 
 
 @admin.register(Task)
@@ -16,6 +16,7 @@ class TaskAdmin(admin.ModelAdmin):
             'fields': ('assigned_to', 'created_by')
         }),
     )
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['task', 'author', 'text', 'created_at', 'text_preview']
     list_filter = ['author', 'created_at']
@@ -25,3 +26,9 @@ class CommentAdmin(admin.ModelAdmin):
     def text_preview(self, obj):
         return obj.text[:50] + "..." if len(obj.text) > 50 else obj.text
     text_preview.short_description = "Comment"
+
+@admin.register(Sprint)
+class SprintAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status', 'start_date', 'end_date', 'task_count', 'completed_tasks', 'created_at', 'updated_at']
+    list_filter = ['status', 'status', 'start_date', 'end_date', 'created_at', 'updated_at']
+    search_fields = ['name', 'goal', 'status']
